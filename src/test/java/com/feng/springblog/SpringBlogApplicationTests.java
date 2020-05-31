587f6434d666f9014638c6e8966b6cdebc7cd9a6
yao00jun <yao00jun@outlook.com>
2020-05-31 15:32:51 +0800

    feat: init

---
+++ src/test/java/com/feng/springblog/SpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feng.springblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
